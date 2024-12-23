d00d633b7ca684efc8e9299f5dab5d0b7b11812f
DavneyRestraDanaz <davney.restra@mhs.itenas.ac.id>
2024-12-23 20:42:55 +0700

    v2.1

--- model/sensor.js
+++ model/sensor.js
@@ -6,14 +6,78 @@ const Sensor = {
         const res = await pool.query('SELECT * FROM sensors');
         return res.rows;
     },
-    create: async (temperature, humidity, water_sensor, sensor_pir, sensor_api) => {
+    
+    create: async (temperature, humidity, water_sensor, motion_sensor, door_locked, door, lamp) => {
         const res = await pool.query(`
-            INSERT INTO sensors (temperature, humidity, water_sensor, sensor_pir, sensor_api, created_at)
-            VALUES ($1, $2, $3, $4, $5, NOW()) -- NOW() untuk timestamp saat ini
+            INSERT INTO sensors (temperature, humidity, water_sensor, motion_sensor, door_locked, door, lamp, created_at)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, NOW())
             RETURNING *;
-        `, [temperature, humidity, water_sensor, sensor_pir, sensor_api]);
+        `, [temperature, humidity, water_sensor, motion_sensor, door_locked, door, lamp]);
         return res.rows[0];
     },
+    
+    // Add the update method
+    update: async (id, data) => {
+        const { temperature, humidity, water_sensor, motion_sensor, door_locked, door, lamp } = data;
+
+        // Build the set clause dynamically based on provided data
+        let setClause = '';
+        let values = [];
+        let counter = 1;
+
+        if (temperature !== undefined) {
+            setClause += `temperature = $${counter}, `;
+            values.push(temperature);
+            counter++;
+        }
+        if (humidity !== undefined) {
+            setClause += `humidity = $${counter}, `;
+            values.push(humidity);
+            counter++;
+        }
+        if (water_sensor !== undefined) {
+            setClause += `water_sensor = $${counter}, `;
+            values.push(water_sensor);
+            counter++;
+        }
+        if (motion_sensor !== undefined) {
+            setClause += `motion_sensor = $${counter}, `;
+            values.push(motion_sensor);
+            counter++;
+        }
+        if (door_locked !== undefined) {
+            setClause += `door_locked = $${counter}, `;
+            values.push(door_locked);
+            counter++;
+        }
+        if (door !== undefined) {
+            setClause += `door = $${counter}, `;
+            values.push(door);
+            counter++;
+        }
+        if (lamp !== undefined) {
+            setClause += `lamp = $${counter}, `;
+            values.push(lamp);
+            counter++;
+        }
+
+        // Remove the last comma and space
+        setClause = setClause.slice(0, -2);
+
+        // Add the id at the end of the values array
+        values.push(id);
+
+        // Execute the query to update the sensor data
+        const res = await pool.query(`
+            UPDATE sensors
+            SET ${setClause}
+            WHERE id = $${counter}
+            RETURNING *;
+        `, values);
+
+        // Return the updated sensor data
+        return res.rows[0];
+    }
 };
 
 module.exports = Sensor;
--- router/sensorRouter.js
+++ router/sensorRouter.js
@@ -16,12 +16,38 @@ router.get('/', async (req, res) => {
 
 // Menambahkan data sensor baru
 router.post('/', async (req, res) => {
-    const { temperature, humidity, water_sensor, sensor_pir, sensor_api } = req.body;
+    const { temperature, humidity, water_sensor, motion_sensor, door_locked, door, lamp } = req.body;
     console.log(req.body);
 
     try {
-        const newSensor = await Sensor.create(temperature, humidity, water_sensor, sensor_pir, sensor_api);
+        const newSensor = await Sensor.create(temperature, humidity, water_sensor, motion_sensor, door_locked, door, lamp);
         res.status(201).json(newSensor);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+// Memperbarui data sensor tertentu (partial update)
+router.patch('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { temperature, humidity, water_sensor, motion_sensor, door_locked, door, lamp } = req.body;
+    
+    try {
+        const updatedSensor = await Sensor.update(id, {
+            temperature,
+            humidity,
+            water_sensor,
+            motion_sensor,
+            door_locked,
+            door,
+            lamp
+        });
+
+        if (!updatedSensor) {
+            return res.status(404).json({ message: 'Sensor not found' });
+        }
+
+        res.json(updatedSensor);
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
